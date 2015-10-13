e9a669ddf132c41f97095f687cc775f936c71882
andre lima <andreloty@outlook.com>
2015-10-13 13:23:17 -0300

    Exemplo de como mandar um json e depois converter pra um objeto c#

--- ServerTeste2/Controllers/ClienteController.cs
+++ ServerTeste2/Controllers/ClienteController.cs
@@ -35,16 +35,21 @@ namespace ServerTeste2.Controllers
 
         [Route("api/addclient")]
         [HttpPost]
-        public HttpResponseMessage CadastroCliente(Cliente cliente, string password)
+        public HttpResponseMessage CadastroCliente(JObject model)
         {
             try
             {
                 //System.Web.Script.Serialization.JavaScriptSerializer js = new System.Web.Script.Serialization.JavaScriptSerializer();
                 //var data = js.Deserialize<Cliente>(dataCliente);
-                db.Database.CreateIfNotExists();
-                //var c = data.cliente;
-                //db.Cliente.Add(c);
-                db.SaveChanges();
+                //db.Database.CreateIfNotExists();
+                ////var c = data.cliente;
+                ////db.Cliente.Add(c);
+                //db.SaveChanges();
+
+                dynamic json = model;
+
+                var cliente = json.cliente.ToObject<Cliente>();
+                var senha = json.password.ToString();
 
                 return Request.CreateResponse(HttpStatusCode.OK);
             }
@@ -55,6 +60,38 @@ namespace ServerTeste2.Controllers
             }
         }
 
-        
+
+        [Route("api/getclient")]
+        public HttpResponseMessage GetCliente()
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+
+            }
+        }
+
     }
 }
+/*
+exemplo de envio: (RAW)
+
+{
+    "password":"minhasenhamalucacriptografada",
+    "cliente": {
+        "nomeCliente": "nomeCliente",
+        "sobrenomeCliente": "sobrenomeCliente",
+        "telefoneCliente": "telefoneCliente",
+        "emailCliente": "emailCliente",
+        "cidadeCliente": "cidadeCliente",
+        "estadoCliente": "estadoCliente",
+        "geoLatitudeCliente": "geoLatitudeCliente",
+        "geoLongitudeCliente": "geoLongitudeCliente"
+    }
+}
+
+*/
